d1493cd6b4a9b869862aa3d4f11427f15a8f00e2
fml <fangmeiliang@cmcm.com>
2019-12-28 12:02:10 +0800

    高阶组件过滤不相关的props传递

--- src/components/A.jsx
+++ src/components/A.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 
-export default(title) => Children => class A extends Component {
+export default(title) => WrappedComponent => class A extends Component {
     render() {
+      // 过滤掉此非高阶组件额外的props(如这里的age, 在包装组件将不显示age), 而且不需要进行透传
+      const { age, ...otherProps } = this.props
+      // 将 props 注入到被包装的组件中。
+      // 通常为 state 的值或者实例方法。
+      const injectedProp = function somefun() { /* ……  */};
+      // 将props传递给被包装组件
       return (
         <div className="header-container">
           <div className="header-top">
             <div style={{color: 'red'}}>{title}</div>
           </div>
           <div className="main">
-            <Children food="小鱼仔" {...this.props}></Children>
+            <WrappedComponent food="小鱼仔" injectedProp={injectedProp} {...otherProps} />
           </div>
         </div>
       )
